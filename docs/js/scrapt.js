3f83cc93c21fc3e2ff9f207ad8f243ab592b4a32
nodlam <nodlam@live.fr>
2018-02-10 03:04:47 +0100

    scrapt
    scrapt

--- docs/js/scrapt.js
+++ docs/js/scrapt.js
@@ -2,18 +2,72 @@ var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
 
-request("https://restaurant.michelin.fr/restaurants/paris/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin", function(error, response, body) {
-  if(error) {
-    console.log("Error: " + error);
-  }
-  console.log("Status code: " + response.statusCode);
+function get_info(url, callback) {
+    request(url, function (err, resp, html) {
+        if (!err) {
+            const $ = cheerio.load(html);
+            var name = $('h1').first().text();
+            var address = $('.thoroughfare').first().text();
+            var zipcode = $('.postal-code').first().text();;
+            var city = $('.locality').first().text();
+            var chef = $('.field--name-field-chef').children('.field__items').text();
+            var restaurant = {
+                "name": name,
+                "address": address,
+                "zipcode": zipcode,
+                "city": city,
+                "chef": chef
+            };
+            callback(restaurant);
+        }
+    });
+}
 
-  var $ = cheerio.load(body);
+function get_urls_on_page(url, callback) {
+    var page_urls = [];
+    request(url, function (err, resp, html) {
+        if (!err) {
+            const $ = cheerio.load(html);
+            $('a[class=poi-card-link]').each(function (i, elem) {
+                page_urls.push('https://restaurant.michelin.fr' + $(elem).attr('href'));
+            }
+            );
+            callback(page_urls);
+        }
+    });
+}
 
-  $('div#ds-1col node node--poi view-mode-poi_card node--poi-card node--poi--poi-card clearfix gtm-jsevent-processed').each(function( index ) {
-    var user = $(this).find('div.attr-gtm-title').text().trim();
-    console.log("User: " + user);
-    fs.appendFileSync('reddit.txt', user + '\n');
-  });
+function get_total_page_number(url, callback) {
+    request(url, function (err, resp, html) {
+        if (!err) {
+            const $ = cheerio.load(html);
+            var nbr = $('ul.pager').children('.last').prev().children().html();
+            callback(nbr);
+        }
+    });
+}
 
-});
+function scrape(url) {
+    var json = { "restaurants": [] };
+    get_total_page_number(url, function (nbr) {
+        for (var i = 1; i < +nbr + 1; i++) {
+            get_urls_on_page(url + '/page-' + i.toString(), function (arr) {
+                arr.forEach(function (elem) {
+                    get_info(elem, function (restaurant) {
+                        json.restaurants.push(restaurant);
+                        fs.writeFile('restaurants.json', JSON.stringify(json), 'utf8', function(err) {
+                            if (!err) {
+                                console.log('One restaurant has been added.');
+                            }
+                            else {
+                                return console.log(err);
+                            }
+                        });
+                    });
+                });
+            });
+        }
+    });
+}
+
+scrape('https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin');
